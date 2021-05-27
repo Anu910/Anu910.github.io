35c62a9c0f6969ae7a8cce3cf22674af47dc9e12
Anu910 <61679599+Anu910@users.noreply.github.com>
2021-05-27 15:14:47 +0530

    Update views.py

--- AllAboutMe/views.py
+++ AllAboutMe/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
+def index(request):
 	return render(request,'AllAboutMe/index.html')
 def about(request):
 	return render(request,'AllAboutMe/about.html')
