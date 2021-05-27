e1399743f30adb43b74c059c3839dbb0a6da0cc0
Anu910 <61679599+Anu910@users.noreply.github.com>
2021-05-27 15:11:01 +0530

    New commit

--- AllAboutMe/views.py
+++ AllAboutMe/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request,'AllAboutMe/homepage.html')
+	return render(request,'AllAboutMe/index.html')
 def about(request):
 	return render(request,'AllAboutMe/about.html')
